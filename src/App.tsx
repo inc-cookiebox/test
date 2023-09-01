f900e2360d875a01f7959316d8562d60af95e695
03hgryan <03hgryan@gmail.com>
2023-09-01 14:47:51 +0900

    fix: to default

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div><div class="title-box"  >
-        <a  >This is a title</a>
+    <div><div class="title-box">
+        <a>This is a title</a>
       </div>
-      <div class="body-box"  >This is a body</div>
-      <div class="body-box"  >This is a body</div>
-      <div class="body-box"  >This is a body</div>
-      <div class="box-box"  >
-        <div class="box"  >
-          <div class="left"  >this is a list left</div>
-          <div class="center"  >this is a list center</div>
-          <div class="right"  >this is a list right</div>
+      <div class="body-box">This is a body</div>
+      <div class="body-box">This is a body</div>
+      <div class="body-box">This is a body</div>
+      <div class="box-box">
+        <div class="box">
+          <div class="left">this is a list left</div>
+          <div class="center">this is a list center</div>
+          <div class="right">this is a list right</div>
         </div>
       </div></div>
   )
