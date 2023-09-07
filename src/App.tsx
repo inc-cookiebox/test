d9da69cef371df3455f325c80f44419c4272fd95
cookieboxbot <143433206+cookieboxbot@users.noreply.github.com>
2023-09-07 10:25:01 +0900

    test: all case - CookieBox

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App() {
 
   return (
     <div><div class="title-box"  >
-        <a style="text-align: left; color: #ffffff;" >This is a title</a>
+        <a style="text-align: right; color: #ffffff;" >This is a title</a>
       </div>
-      <div class="body-box"  style="text-align: center;">This is a body</div>
-      <div class="body-box"  >This is a body</div>
-      <div class="body-box"  >This is a body</div>
+      <div class="body-box" style="text-align: center;" >This is a body</div>
+      <div class="body-box"  style="text-align: right;">This is a body</div>
+      <div class="body-box"  style="text-align: left;">This is a body</div>
       <div class="box-box"  >
         <div class="box"  >
           <div class="left"  >this is a list left</div>
