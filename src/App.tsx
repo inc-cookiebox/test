2d2693cd29830b34e31cc31552c594713f1a52fd
cookieboxbot <143433206+cookieboxbot@users.noreply.github.com>
2023-10-24 15:05:02 -0700

    test: test-react inline style - CookieBox

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,25 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <div className="title-box">
-        <a style={{ color: "#ffffff", textAlign: "right" }}>This is a title</a>
+    <div><div class="title-box"  >
+        <a style="color: #ffffff; text-align: right;" >This is a title</a>
       </div>
-      <div className="body-box" style={{ textAlign: "center" }}>
+      <div class="body-box" style="text-align: left;" >
         This is a body
       </div>
-      <div className="body-box" style={{ textAlign: "center" }}>
+      <div class="body-box" style="text-align: right;" >
         This is a body
       </div>
-      <div className="body-box" style={{ textAlign: "left" }}>
+      <div class="body-box" style="text-align: left;" >
         This is a body
       </div>
-      <div className="box-box">
-        <div className="box">
-          <div className="left">this is a list left</div>
-          <div className="center">this is a list center</div>
-          <div className="right">this is a list right</div>
+      <div class="box-box"  >
+        <div class="box"  >
+          <div class="left"  >this is a list left</div>
+          <div class="center"  >this is a list center</div>
+          <div class="right"  >this is a list right</div>
         </div>
-      </div>
-    </div>
+      </div></div>
   );
 }
 
