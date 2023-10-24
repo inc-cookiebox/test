9d68fb54d8c73a8a9746b9942c673f99b9ed53e4
03hgryan <03hgryan@gmail.com>
2023-10-24 13:39:50 -0700

    fix: react inline style

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <div><div class="title-box"  >
-        <a style="text-align: right; color: #ffffff;" >This is a title</a>
+    <div>
+      <div className="title-box">
+        <a style={{ color: "#ffffff", textAlign: "right" }}>This is a title</a>
       </div>
-      <div class="body-box" style="text-align: center;" >This is a body</div>
-      <div class="body-box" style="text-align: center;" >This is a body</div>
-      <div class="body-box" style="text-align: left;" >This is a body</div>
-      <div class="box-box"  >
-        <div class="box"  >
-          <div class="left"  >this is a list left</div>
-          <div class="center"  >this is a list center</div>
-          <div class="right"  >this is a list right</div>
+      <div className="body-box" style={{ textAlign: "center" }}>
+        This is a body
+      </div>
+      <div className="body-box" style={{ textAlign: "center" }}>
+        This is a body
+      </div>
+      <div className="body-box" style={{ textAlign: "left" }}>
+        This is a body
+      </div>
+      <div className="box-box">
+        <div className="box">
+          <div className="left">this is a list left</div>
+          <div className="center">this is a list center</div>
+          <div className="right">this is a list right</div>
         </div>
-      </div></div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
